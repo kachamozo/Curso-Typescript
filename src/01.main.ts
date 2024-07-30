1149cd39682becdc62b758ec03e1680fa214c851
kachamozo <cachamocito@yahoo.com>
2024-07-29 23:48:11 -0400

    fix html title

--- src/01.main.ts
+++ src/01.main.ts
@@ -9,5 +9,5 @@ if (num1 && num2) {
 	alert(sumarNumeros(parseInt(num1), parseInt(num2)));
 }
 
-//Cuando tsc compila el codigo de cada archivo lo lleva a un scope global, por tanto si declaramos variables con el mismo nombre en diferentes archivos nos va a salir un error por ende solo tenemos que colocar export default al final de cada archivo para delimitar que son modulos separados
+//Cuando tsc compila el codigo de cada archivo lo lleva a un scope global, por tanto si declaramos variables con el mismo nombre en diferentes archivos nos va a salir un error por ende solo tenemos que colocar export default al final de cada archivo para delimitar que son modulos separados.
 export default {};
